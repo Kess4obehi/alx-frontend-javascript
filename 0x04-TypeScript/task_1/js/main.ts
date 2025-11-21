9ebf8bfd24e76efb937c1b8b1ededdba40f2cd65
kess4obehi <kessiena4excellence@gmail.com>
2025-11-21 16:37:41 +0100

    created a class named StudentClass that implements a studentclassinterface

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -21,3 +21,32 @@ function printTeacher({ firstName, lastName }: { firstName: string; lastName: st
 
 export { printTeacher };
 
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+class StudentClass implements StudentClassInterface {
+  // 1. The storage spots (Properties)
+  firstName: string;
+  lastName: string;
+
+  // 2. The Builder (Constructor)
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+  // 3. The Actions (Methods)
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
